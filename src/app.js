aabd16270b0058288c0e44fee5ec351c79295094
Camilo Cortes <100862481+camilocortes27@users.noreply.github.com>
2024-10-11 10:53:00 +0000

    fisrt commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,49 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  // Arrays con la excusa
+  let quien = [
+    "Mi perro ",
+    "El vecino ",
+    "Mi abuela ",
+    "Mi gato ",
+    "Mis padres "
+  ];
+  let accion = [
+    "se comió ",
+    "rompió ",
+    "destruyó ",
+    "robó ",
+    "desaparecio ",
+    "tiro "
+  ];
+  let objeto = [
+    "mi tarea ",
+    "el auto ",
+    "mi celular ",
+    "la llave ",
+    "mi bolso ",
+    "mi scooter "
+  ];
+  let cuando = [
+    "esta mañana ",
+    "ayer ",
+    "la semana pasada ",
+    "hace un momento ",
+    "anoche ",
+    "saliendo para aca "
+  ];
+
+  // organizador aleatorio de la excusa
+  let aleatorioquien = Math.floor(Math.random() * quien.length);
+  let aletorioaccion = Math.floor(Math.random() * accion.length);
+  let aleatorioobjeto = Math.floor(Math.random() * objeto.length);
+  let aletoriocuando = Math.floor(Math.random() * cuando.length);
+
+  // Generar la excusa
+  document.querySelector("#excusa").innerHTML =
+    quien[aleatorioquien] +
+    accion[aletorioaccion] +
+    objeto[aleatorioobjeto] +
+    cuando[aletoriocuando];
 };
